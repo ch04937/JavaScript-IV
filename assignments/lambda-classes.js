4ae4142d742e742089422c80071ec7c2b336c746
unknown <ch04937>
2019-06-13 14:42:16 -0700

    created intructor class that extends to Person

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -4,8 +4,23 @@ class Person{
         this.name= baseClass.name;
         this.age = baseClass.age;
         this.location = baseClass.location;
-        speaks(){
-            return `Hello my name is ${baseClass.name}, I am from ${baseClass.location} and I am ${baseClass.age}`;
+    }        
+    speaks(){
+        return `Hello my name is ${baseClass.name}, I am from ${baseClass.location} and I am ${baseClass.age}`;
         }
+
+}
+class Instructor extends Person{
+    constructor(instructorBaseClass){
+        super(this, instructorBaseClass);
+        this.specialty = instructorBaseClass.specialty;
+        this.favLanguage = instructorBaseClass.favLanguage;
+        this.catchPhrase = instructorBaseClass.catchPhrase;
+    }
+    demo(subject){
+        return `'Today we are learning about ${subject}' where subject is the param passed in.`;
+    }
+    grade(student,subject){
+        return `'${student.name} receives a perfect score on ${subject}'`
     }
 }
