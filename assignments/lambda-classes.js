2aaa3b54864810c6184f4af0b05431cc15ab9b73
unknown <ch04937>
2019-06-13 15:13:09 -0700

    created class Project Manager that extends to intructor and class student that extends to person

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -24,3 +24,33 @@ class Instructor extends Person{
         return `'${student.name} receives a perfect score on ${subject}'`
     }
 }
+class Student extends Person{
+    constructor(studentBaseClass){
+        super(this, studentBaseClass);
+        this.previousBackground = studentBaseClass.previousBackground;
+        this.className = studentBaseClass.className;
+        this.favSubject = studentBaseClass.favSubject;
+    }
+    listsSubjects(){
+        return `${studentBaseClass.favSubject} is ${baseClass.name} favorite subject`;
+    }
+    PRAssignment(subject){
+        return `${student.name} has submitted a PR for ${subject}`;
+    }
+    sprintChallenge(){
+        return `${student.name} has begun sprint challenge on ${subject}`;
+    }
+}
+class ProjectManagers extends Instructor{
+    constructor(projectManagerInstructorBaseClass){
+        super(this, projectManagerInstructorBaseClass);
+        this.gradeClass = projectManagerInstructorBaseClass.gradeClass;
+        this.favInstructor = projectManagerInstructorBaseClass.favInstructor;
+    }
+    standUp(){
+        return `${name} announces to ${channel}, @channel standy times!`
+    }
+    debugsCode(student, subject){
+        return `${name} debugs ${student.name}'s code on ${subject}`;
+    }
+}
