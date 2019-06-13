09933b34eb8cf055f92702ef47725780b6fcface
unknown <ch04937>
2019-06-13 14:32:04 -0700

    created person class and constructor

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,11 @@
 // CODE here for your Lambda Classes
+class Person{
+    constructor(baseClass){
+        this.name= baseClass.name;
+        this.age = baseClass.age;
+        this.location = baseClass.location;
+        speaks(){
+            return `Hello my name is ${baseClass.name}, I am from ${baseClass.location} and I am ${baseClass.age}`;
+        }
+    }
+}
